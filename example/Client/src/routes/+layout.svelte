c72b79c518bf19525b0833e5d4ca36282bcf1cca
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-05-03 16:38:08 +0200

    Added svelte project

---
+++ example/Client/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css?family=Mulish" rel="stylesheet">
+</svelte:head>
+
+<script>
+    import "../app.css";
+  </script>
+  
+  <slot />
---
+++ example/Client/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<h1 class="text-s">Welcome to SvelteKit</h1>
+<script>
+	const fetchImage = (async () => {
+		const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    return await response.json()
+	})()
+</script>
+
+<div class="flex flex-row">
+
+<div class="flex flex-col">
+    <label class="text-3xl" for="quantity">Quantity (between 1 and 5):</label>
+    <input type="number" id="weatherId" min="1" step="1">
+</div>
+
+{#await fetchImage}
+	<p>...waiting</p>
+{:then data}
+	<img src={data.message} alt="Dog image" />
+{:catch error}
+	<p>An error occurred!</p>
+{/await}
+
+</div>
